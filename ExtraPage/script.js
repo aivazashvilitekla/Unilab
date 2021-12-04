ecadec0da2b5b5915e31db35ca55683be506dd35
aivazashvilitekla <tekla.aivazashvili@gau.edu.ge>
2021-12-04 14:56:40 +0400

    finished api task

--- ExtraPage/script.js
+++ ExtraPage/script.js
@@ -1,23 +1,8 @@
 
 
-
-
-
-
-
 let table = document.getElementById('usersTable');
-// arrays
-// colors = ['#00B45C', '#EA0049', '#FEC900', '#00A3BA']
-names = ['Berthefried', 'Tatiana', 'Hildeburg', 'Bilbo'];
-Snames = ['Baggins', 'Lightfoot', 'Boulderhill', 'Frodo'];
-function getStatus() {
-    return Math.floor((Math.random() * 2) + 1) == 1 ? 'Active' : 'Inactive';
-}
+
 function getPosts() {
-    // let data = {
-    //     userCount,
-        
-    // }
     fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'GET',
         headers: {
@@ -28,73 +13,37 @@ function getPosts() {
         return response.json();
     })
     .then(function (respData) {
-        console.log(respData);
-        let arr = respData.map(item => {
-            return item.userId;
-        })
-        let userIDs = [... new Set(arr)];
-        console.log(userIDs);
-        for (let i = 1; i <= userIDs.length; i++){
-            let rand = Math.floor(Math.random() * 4);
+        for (let i = 1; i <= respData.length; i++){
             var row = table.insertRow(i);
-            // row.style.background = colors[rand];
-
-            // ID
+            // Userid
             var cell1 = row.insertCell(0);
-            cell1.innerHTML = i;
+            cell1.innerHTML = respData[i].userId;
             
             
-            // Firstname
+            // // id
             var cell2 = row.insertCell(1);
-            cell2.innerHTML = names[rand];
+            cell2.innerHTML = respData[i].id;
 
-            // Lastname
+            // // title
             var cell3 = row.insertCell(2);
-            cell3.innerHTML = Snames[rand];
+            cell3.innerHTML = respData[i].title;
 
-            // Status
+            // // body
             var cell4 = row.insertCell(3);
-            cell4.innerHTML = getStatus();
-            if (getStatus() === 'Active') {
-                row.style.background = '#2bb52b';
-            } else {
-                row.style.background = '#e08566';
-            }
-            for (let j = 0; j <= 10; j++){
-                var row = table.insertRow(i);
-            }
+            cell4.innerHTML = respData[i].body;
             
-            // var cell5 = row.insertCell(4);
-            // cell5.innerHTML = "<button type='button' class='btn' onClick='openPosts()'>See Posts</button>";
-            // cell5.style.textAlign = 'center';
+
+            // let arr = respData.filter(item => {
+            //     return item.userId==i;
+            // })
+            // console.log(arr);
         }
+        
         
     })
     .catch(function (error) {
         console.log(error);
     })
-    return
 }
 
 getPosts()
-function openPosts() {
-    let usersDiv = document.getElementById('users');
-    let newDiv = document.createElement('div');
-
-    newDiv.classList.add('modal');
-    newDiv.classList.add('myModal');
-    usersDiv.appendChild(newDiv);
-    newDiv.innerHTML = `<div class="modal-content">
-                        <span class="close">&times;</span>
-                        <p>Some text in the Modal..</p>
-                        </div>`;
-    let modal = document.getElementsByClassName('modal');
-    modal.style.display = "block";
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-    }
-}
-// 
